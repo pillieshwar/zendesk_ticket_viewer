af0fc9f4bc4e675efa00152852b08852e9ba06d9
pillieshwar <pillieshwar@gmail.com>
2021-11-23 02:28:58 -0800

    added ticket count for pagination

--- src/main/java/com/example/demo/ZendeskApplication.java
+++ src/main/java/com/example/demo/ZendeskApplication.java
@@ -173,4 +173,60 @@ public class ZendeskApplication {
 		return ticketsArr.toString();
 	}
 
+	@CrossOrigin
+	@GetMapping("/ticketCount")
+	public String getTicketCount(){
+		Scanner sc;
+		System.out.println("inside Ticket Count");
+		String email_address = "eshwarnag.pilli@wsu.edu";
+		String password = "Eswar123$";
+		String stringURL = "https://zcceshwar.zendesk.com/api/v2/tickets/count.json";
+
+		String basicAuth = "";
+		String ticketsJSON = "";
+		JSONObject jsonObject;
+		JSONObject ticket1;
+		URL url;
+		try {
+		url = new URL(stringURL);
+		URLConnection urlConnection = url.openConnection();
+				
+		String userAuthentication = email_address + ":" + password;
+		basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userAuthentication.getBytes());
+		
+		urlConnection.setRequestProperty ("Authorization", basicAuth);
+		
+		urlConnection.connect();
+		InputStream inputStream = urlConnection.getInputStream();
+		
+		sc = new Scanner(inputStream);
+		sc = sc.useDelimiter("\\A");
+		String result = sc.hasNext() ? sc.next() : "";
+		sc.close();
+		ticketsJSON = result;
+		
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		Customer customer = objectMapper.readValue(result);
+//		JSONObject ticketsJSON = new JSONObject();
+		jsonObject= new JSONObject(result);
+		ticket1 = new JSONObject();
+		ticket1 = jsonObject.getJSONObject("count");
+//		Date date = new Date();
+//		String dateStr = "";
+//		date = dateFormat.parse(ticket1.getString("updated_at"));
+//		ticket1.put("updated_at", date);
+//		ticket1.put("requester_id", ticket1.getInt("requester_id"));
+//		 String str = jsonObject.getString("ticket");
+		System.out.println(ticket1);
+		}
+		catch (MalformedURLException e) {
+			System.out.println("ERROR: Failed connection. Malformed URL.");
+			return null;
+			
+		} catch (Exception e) {
+			System.out.println("ERROR: Ooops! Something went wrong fetching your ticket(s).");
+			return null;
+		}
+		return ticket1.toString();
+	}
 }
